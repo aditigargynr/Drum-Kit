2c3032e0ed26c80328169cef2823784395cc7aba
aditigargynr <82716794+aditigargynr@users.noreply.github.com>
2021-05-30 14:17:17 +0530

    Added initial Drum-Kit Files

---
+++ index.js
@@ -0,0 +1,79 @@
+
+function soundplay(letter)
+{
+    switch (letter) 
+    {
+    case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+    case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+    case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+    case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+    case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+    case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+    case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+    default:
+        alert("You pressed " + letter + " !");
+        break;
+    }    
+}
+
+function activeButton(currentKey)
+{
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+       activeButton.classList.remove("pressed");
+    },100);
+}
+
+for(var n=0 ;  n < document.querySelectorAll(".drum").length ; n++)
+{
+document.querySelectorAll("button")[n].addEventListener("click",function() 
+    {
+        var buttonClicked = this.innerHTML;
+        soundplay(buttonClicked) ;
+        activeButton(buttonClicked);
+    } );
+}
+
+
+document.addEventListener("keydown",function(event)
+{
+
+    soundplay(event.key);
+    activeButton(event.key);
+});
+
+
+
+
+
+
